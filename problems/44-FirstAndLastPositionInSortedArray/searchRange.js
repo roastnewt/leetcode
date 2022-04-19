95d6bbcf2c725f576ee92b9c1297dfbc8c4756f3
Clayton Watterson <cwatterson@gmail.com>
2022-04-19 13:02:31 -0400

    Another solution to searchRange

--- problems/44-FirstAndLastPositionInSortedArray/searchRange.js
+++ problems/44-FirstAndLastPositionInSortedArray/searchRange.js
@@ -1,3 +1,5 @@
+// Normally O(log(n) + C), but worst case O(n)
+
 var searchRange = function(nums, target) {
 
   let start = 0;
@@ -24,3 +26,46 @@ var searchRange = function(nums, target) {
 };
 
 module.exports = searchRange;
+
+
+
+// Another solution - two binary searches.  Always O(log(n)).  May be slower in practice
+// const searchRange = function(nums, target) {
+//     let leftMost = -1;
+//     let rightMost = -1;
+
+//     let l = 0;
+//     let r = nums.length - 1;
+//     while (l <= r) {
+//         let mid = Math.floor((l + r) / 2);
+
+//         if (nums[mid] === target) {
+//             leftMost = mid;
+//             r = mid - 1
+//         } else if (nums[mid] > target) {
+//             r = mid - 1;
+//         } else {
+//             l = mid + 1;
+//         }
+//     }
+
+//     if (leftMost === -1) return [-1, -1];
+
+//     // dont need to reset left
+//     r = nums.length - 1
+
+//     while (l <= r) {
+//         let mid = Math.floor((l + r) / 2);
+
+//         if (nums[mid] === target) {
+//             rightMost = mid;
+//             l = mid + 1
+//         } else if (nums[mid] > target) {
+//             r = mid - 1;
+//         } else {
+//             l = mid + 1;
+//         }
+//     }
+
+//     return [leftMost, rightMost]
+// };
